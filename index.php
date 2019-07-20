0f9db58de1b9c6d3678a647e1c64177195626fa9
Aditya_kumawat <31047132+AdityaKumawat97@users.noreply.github.com>
2019-07-20 13:29:51 +0600

    Play Bar added

--- index.php
+++ index.php
@@ -9,10 +9,64 @@ include("includes/config.php");
  ?>
 <html>
 <head>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 	<title>	</title>
 </head>
 <body>
-	HELLO
+	<div id="nowPlayingBarContainer">
+		<div id="nowPlayingBar">
+			<div id="nowPlayingLeft">
+				
+			</div>
+			<div id="nowPlayingCenter">
+				<div class="content playerControls">
+					
+					<div class="buttons">
+						<button class="controlButton shuffle" title="Shuffle Button">
+							<img src="assets/images/icons/shuffle.png" alt="Shuffle">
+						</button>
+
+						<button class="controlButton previous" title="Shuffle Button">
+							<img src="assets/images/icons/previous.png" alt="Previous">
+						</button>
+
+						<button class="controlButton play" title="Shuffle Button">
+							<img src="assets/images/icons/play.png" alt="Play">
+						</button>
+
+							<button class="controlButton pause" title="Shuffle Button" style="display:none;">
+							<img src="assets/images/icons/pause.png" alt="Pause">
+						</button>
+
+						<button class="controlButton next" title="Shuffle Button">
+							<img src="assets/images/icons/next.png" alt="Next">
+						</button>
+
+						<button class="controlButton repeat" title="Shuffle Button">
+							<img src="assets/images/icons/repeat.png" alt="Repeat">
+						</button>
+					</div>
+					<div class="playbackBar">
+						<span class="progressTime current">
+							0.00
+						</span>
+						<div class="progressBar">
+							<div class="progressBarBg">
+								<div class="progress"></div>
+							</div>
+						</div>
+						<span class="progressTime remaining">0.00</span>
+					</div>
+
+				</div>
+
+
+			</div>
+			<div id="nowPlayingRight">
+				
+			</div>	
+		</div> 
+	</div>
 
 </body>
 </html>
